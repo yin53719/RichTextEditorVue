cb0083b8dd33b9af01b421cb4643f50702830321
yin53719 <yin53719@qq.com>
2019-06-03 17:47:32 +0800

    add file

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-
+import rich from './views/rich.vue';
 Vue.use(Router);
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/rich',
+      name: 'rich',
+      component: rich,
     },
     {
       path: '/about',
---
+++ src/views/rich.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div id="editor">
+            <iframe :src="src">
+
+                sdfsdgf
+            </iframe>
+        </div>
+
+       <el-button @click="handleGetPoint"> 获取光标位置 </el-button>
+       <el-button @click="setContent"> 光标位置插入文字 </el-button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'aaaaa',
+    data(){
+        return {
+            ue:'',
+            src:'./richContent.html'
+        }
+    },
+    created(){
+    //   window.UE.delEditor('editor');
+    },
+    mounted(){
+        
+    },
+    methods:{
+        handleGetPoint(){
+            let textDom = window.document.getElementById('editor')
+            let anchorNode = this.getSelection().anchorOffset
+
+            console.log(anchorNode)
+
+        },
+        getSelection(){
+            // 获取光标位置 anchorOffset 最后位置
+            //文本 anchorNode
+            //  选择位置开始的地方 focusOffset
+            let sel = window.getSelection();
+            console.log(sel)
+            return sel;
+        },
+        setContent(content){
+            this.getSelection().getRangeAt(0).insertNode(document.createTextNode("<p>插入的值</p>"))
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+   #editor{
+       width:50%;
+       height:500px;
+       border: 1px solid #ccc;
+       margin: 20px;
+   }
+</style>
